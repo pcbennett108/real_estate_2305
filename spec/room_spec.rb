9feb5f38a4cbb2bc91828fbcb36460b4ae12a7e3
pcbennett108 <pcbennett42@gmail.com>
2023-05-18 09:51:44 -0600

    Add tests for painting methods

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -21,6 +21,29 @@ RSpec.describe Room do
       expect(room2.area).to eq(180)
     end
 
+    it "checks if room is painted" do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+
+      expect(room1.is_painted?).to be_false
+      expect(room2.is_painted?).to be_false
+
+    end
+
+    xit "can paint the room" do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+
+      expect(room1.is_painted?).to be_false
+      expect(room2.is_painted?).to be_false
+      
+      room1.paint
+      room2.paint
+      
+            expect(room1.is_painted?).to be_true
+            expect(room2.is_painted?).to be_true
+    end
+
 
 
 
